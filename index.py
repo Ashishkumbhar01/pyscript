1f9adf08cbb08a9704c787d734b6c8ebdcf69a76
ASHISH KUMBHAR <53786138+Ashishkumbhar01@users.noreply.github.com>
2022-05-14 05:46:23 +0530

    PyScript
    how to write Your first programm in pyscript in html

---
+++ index.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>PY-Script</title>
+<link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+<script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+</head>
+<body>
+<h1 align="center" style="margin:10px;">Py-Script</h1>
+<py-script>
+print('Hey I am CWS')
+</py-script>
+</body>
+</html>
